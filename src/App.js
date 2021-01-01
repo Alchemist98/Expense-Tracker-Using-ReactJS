6e09947ed0a061b2ab5199d1c0a0470668e538a8
shubham <pshubham1886@gmail.com>
2021-01-01 21:08:54 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import AddTranscation from './components/AddTranscation';
+import Balance from './components/Balance';
+import Header from './components/Header';
+import IncomeExpenses from './components/IncomeExpenses';
+import TranscationList from './components/TranscationList';
+import {GlobalProvider} from './context/GlobalState';
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TranscationList />
+        <AddTranscation />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
